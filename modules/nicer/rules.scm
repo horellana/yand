80785408e34f23086e64c0de681794a0ff0a63a9
Hector Orellana <hector.orellanaf92@gmail.com>
2025-12-25 14:26:03 -0300

    match procs by name and/or argv

--- modules/nicer/rules.scm
+++ modules/nicer/rules.scm
@@ -1,21 +1,39 @@
 (define-module (nicer rules)
+  :use-module (srfi srfi-9)
+  :use-module (srfi srfi-1)
   :export (make-rule match-rule make-process))
 
-(use-modules (srfi srfi-9))
-
 (define-record-type <process>
-  (make-process name)
+  (make-process name args)
   process?
-  (name process-name))
+  (name process-name)
+  (args process-args))
 
 (define-record-type <rule>
-  (make-rule name nice)
+  (make-rule name args nice)
   rule?
   (name rule-name)
+  (args rule-args)
   (nice rule-nice))
 
 (define (match-rule rule process)
-  (if (eq? (rule-name rule)
-	     (process-name process))
+  (if (> (length (rule-args rule)) 0)
+      (match-by-cli-args rule process)
+      (match-by-name rule process)))
+
+(define (match-cli-args rule-args process-args)
+  (lset<= string=? rule-args process-args))
+
+(define (match-by-name rule process)
+  (if (eq? (rule-name rule)      
+	   (process-name process))
       (rule-nice rule)
       #f))
+
+(define (match-by-cli-args rule process)
+  (if (and (match-by-name rule process)
+	   (match-cli-args (rule-args rule) (process-args process)))
+      (rule-nice rule)
+      #f))
+
+
--- tests/rules.scm
+++ tests/rules.scm
@@ -5,16 +5,28 @@
 
 (test-begin "rules")
 
-(let ((emacs-rule (make-rule 'emacs -5))
-      (process (make-process 'emacs)))
+(test-equal "helper: matches cli args correctly"
+  #t
+  ((@@ (nicer rules) match-cli-args)
+   (list "--compile")
+   (list "--compile" "--no-site-lisp")))
+
+(let ((emacs-rule (make-rule 'emacs (list) -5))
+      (process (make-process 'emacs (list))))
   (test-equal "match rule by name"
     -5
     (match-rule emacs-rule process)))
 
-(let ((emacs-rule (make-rule 'emacs -5 (list "compile")))
+(let ((emacs-rule (make-rule 'emacs (list "compile") -5))
       (process (make-process 'emacs (list "compile"))))
   (test-equal "match rule by name and command line args"
     -5
+    (match-rule emacs-rule process)))
+
+(let ((emacs-rule (make-rule 'emacs (list "--compile") -5))
+      (process (make-process 'emacs (list "--debug"))))
+  (test-equal "does match rule by name and command line args if process was launched with different args"
+    #f
     (match-rule emacs-rule process)))
 
 (test-end "rules")
